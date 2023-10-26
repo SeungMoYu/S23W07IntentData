e338cb74c18be3032aa97d6b8a244bca41f462e4
SeungMoYu <smy0917@naver.com>
2023-10-27 01:02:27 +0900

    calle to caller data forwarding

--- app/src/main/java/kr/ac/kumoh/ce/s23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s23w07intentdata/ImageActivity.kt
@@ -1,21 +1,49 @@
 package kr.ac.kumoh.ce.s23w07intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import kr.ac.kumoh.ce.s23w07intentdata.databinding.ActivityImageBinding
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity(), OnClickListener {
+    companion object {
+        const val IMAGE_NAME = "image name"
+        const val IMAGE_RESULT = "image result"
+
+        const val LIKE = 100
+        const val DISLIKE = 101
+        const val NONE = 0
+    }
     private lateinit var main: ActivityImageBinding
+    private lateinit var animal: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityImageBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        animal = intent.getStringExtra(MainActivity.KEY_NAME) ?: "none"
         val res = when (intent.getStringExtra(MainActivity.KEY_NAME)) {
             MainActivity.DOG -> R.drawable.dog
             MainActivity.CAT -> R.drawable.cat
             else -> R.drawable.ic_launcher_foreground
         }
         main.image.setImageResource(res)
+        main.btnLike.setOnClickListener(this)
+        main.btnDislike.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent()
+        val value = when (v?.id){
+            main.btnLike.id-> LIKE
+            main.btnDislike.id-> DISLIKE
+            else-> NONE
+        }
+        intent.putExtra(IMAGE_NAME, animal)
+        intent.putExtra(IMAGE_RESULT, value)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s23w07intentdata/MainActivity.kt
@@ -2,11 +2,13 @@ package kr.ac.kumoh.ce.s23w07intentdata
 
 import android.app.Activity
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.ce.s23w07intentdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnClickListener {
@@ -16,6 +18,25 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         const val DOG = "dog"
     }
     private lateinit var main: ActivityMainBinding
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {
+        if(it.resultCode != Activity.RESULT_OK)
+            return@registerForActivityResult
+
+        val result = it.data?.getIntExtra(
+            ImageActivity.IMAGE_RESULT,
+            ImageActivity.NONE)
+        val str = when(result) {
+            ImageActivity.LIKE -> "좋아요"
+            ImageActivity.DISLIKE -> "싫어요"
+            else -> ""
+        }
+        when(it.data?.getStringExtra(ImageActivity.IMAGE_NAME)) {
+            CAT -> main.btnCat.text = "고양이 ($str)"
+            DOG -> main.btnDog.text = "강아지 ($str)"
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
@@ -39,6 +60,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             else -> return
         }
         intent.putExtra(KEY_NAME, value)
-        startActivity(intent)
+        //startActivity(intent)
+        startForResult.launch(intent)
     }
 }
